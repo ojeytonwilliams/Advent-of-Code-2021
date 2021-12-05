79a174f4f7b63cbd07079b3519202738d2ab4910
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-05 19:48:10 +0100

    feat: Advent day 1 part 2

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@
         static void Main(string[] args)
         {
 
-    string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
-    int[] xs = lines.Select(x => Convert.ToInt32(x)).ToArray();
-            int previous = xs[0];
+            string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
+            int[] xs = lines.Select(x => Convert.ToInt32(x)).ToArray();
+            int previous = xs[0] + xs[1] + xs[2];
             int count = 0;
-            foreach (int i in xs)
+
+            for (int i = 3; i < xs.Length; i++)
             {
-                if (i > previous) {
-                    count++;
-                }
-                previous = i;
+                int sum = xs[i - 2] + xs[i - 1] + xs[i];
+                if (sum > previous) count++;
+                previous = sum;
             }
             Console.Write(count);
         }
