73ab5297585572b79dcf3e315a26235055e4fc41
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-05 19:42:46 +0100

    feat: Advent day 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+    string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
+    int[] xs = lines.Select(x => Convert.ToInt32(x)).ToArray();
+            int previous = xs[0];
+            int count = 0;
+            foreach (int i in xs)
+            {
+                if (i > previous) {
+                    count++;
+                }
+                previous = i;
+            }
+            Console.Write(count);
+        }
+
+    }
+}
