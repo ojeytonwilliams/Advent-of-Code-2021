c1fffb0d9c56bbc3cf526332a61fc493a02cb74b
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-05 20:03:17 +0100

    feat: Advent day 2 part 2

--- Program.cs
+++ Program.cs
@@ -8,15 +8,17 @@
             string[] lines = System.IO.File.ReadAllLines(@".\input.txt");
             int forward = 0;
             int depth = 0;
+            int aim = 0;
             foreach (string line in lines)
             {
                 int amount = Convert.ToInt32(line.Split(" ")[1]);
-                if (line[0] == "f"[0]) forward += amount;
-
-                if (line[0] == "u"[0])depth -= amount;
-                
-                if (line[0] == "d"[0])depth += amount;
-                
+                if (line[0] == "f"[0])
+                {
+                    forward += amount;
+                    depth += aim * amount;
+                }
+                if (line[0] == "u"[0]) aim -= amount;
+                if (line[0] == "d"[0]) aim += amount;
             }
             Console.Write(forward * depth);
         }
