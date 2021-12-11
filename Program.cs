8c8a54c1e93559ac4ea8d6e845676eda1939b927
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-12-11 20:46:40 +0100

    feat: Advent day 4 part 1

--- Program.cs
+++ Program.cs
@@ -6,65 +6,126 @@
         {
 
             var lines = System.IO.File.ReadAllLines(@".\input.txt");
-            var len = lines[0].Length;
+            int[] numbers = lines[0].Split(",").Select(x => Convert.ToInt32(x)).ToArray();
+            string[] boardData = new string[
+                lines.Length - 1
+            ];
+            Array.Copy(lines, 1, boardData, 0, lines.Length - 1);
+            IEnumerator<string> lineEnumerator = boardData.AsEnumerable().GetEnumerator();
+            var boardStack = new Stack<int[][]>();
 
-            bool isOneMostCommon(int[] lines, int position)
+            while (lineEnumerator.MoveNext())
             {
-                var (zero, one) = countBits(lines, position);
-                return one >= zero;
+                boardStack.Push(parseBoards(lineEnumerator));
             }
 
-            int[] filtered = lines.Select(x => Convert.ToInt32(x, fromBase: 2)).ToArray();
-            for (int i = len - 1; i >= 0; i--)
+            var boards = boardStack.ToArray();
+            
+            foreach (int n in numbers)
             {
-                filtered = filterByRequiredBit(filtered, i, isOneMostCommon);
-            }
-
-            int oxygen = filtered[0];
-
-            bool isOneLeastCommon(int[] lines, int position) => !isOneMostCommon(lines, position);
-
-            filtered = lines.Select(x => Convert.ToInt32(x, fromBase: 2)).ToArray();
-            for (int i = len - 1; i >= 0; i--)
-            {
-                filtered = filterByRequiredBit(filtered, i, isOneLeastCommon);
-            }
-
-            int cO2 = filtered[0];
-
-            Console.WriteLine(oxygen * cO2);
-
-
-            bool hasOne(int num, int position)
-            {
-                return (num >> (position) & 1) == 1;
-            }
-
-            (int zero, int one) countBits(int[] lines, int n)
-            {
-                int zero = 0;
-                int one = 0;
-                foreach (var num in lines)
+                foreach (var board in boards)
                 {
-                    if (hasOne(num, n))
-                    {
-                        one++;
-                    }
-                    else
-                    {
-                        zero++;
+                    markPosition(board, n);
+                    if(isWinner(board)) {
+                        Console.WriteLine(getScore(board, n));
+                        Environment.Exit(0);
                     }
                 }
-                return (zero, one);
+            }
+            
+            int getScore(int[][] board, int num) {
+                int score = 0;
+                foreach(var row in board) {
+                    foreach(int n in row) {
+                        if(n > 0) {
+                            score += n;
+                        }
+                    }
+                }
+                return score * num;
             }
 
-            int[] filterByRequiredBit(int[] lines, int n, Func<int[], int, bool> findFilterBit)
+            void prettyPrint<T>(T[][] board)
             {
-                if (lines.Length == 1) return lines;
-                Func<int, int,bool> predicate = findFilterBit(lines, n) ? (x, n) => hasOne(x, n) : (x, n) => !hasOne(x, n);
-                return lines.Where(x => predicate(x, n)).ToArray();
-        }
+                foreach (var item in board)
+                {
+                    Console.WriteLine(String.Join(",", item));
+                }
+            }
 
+
+
+            void markPosition(int[][] board, int n)
+            {
+                for (int i = 0; i < board.Length; i++)
+                {
+                    var id = Array.IndexOf(board[i], n);
+                    if (id > -1)
+                    {
+                        board[i][id] = -1;
+                    }
+                }
+            }
+
+            bool isWinner(int[][] board)
+            {
+                if (hasFullRow(board)) return true;
+                if (hasFullColumn(board)) return true;
+                return false;
+            }
+
+            bool hasFullRow(int[][] board)
+            {
+                foreach (var item in board)
+                {
+                    if (Array.TrueForAll(item, (int x) => x == -1))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            bool hasFullColumn(int[][] board)
+            {
+                for (int col = 0; col < board.Length; col++)
+                {
+                    bool full = true;
+                    for (int row = 0; row < board.Length; row++)
+                    {
+                        if (board[row][col] > -1)
+                        {
+                            full = false;
+                        }
+                    }
+                    if (full) return true;
+                }
+                return false;
+            }
+
+            // (int, int) findPosition(int[][] board, int n) {
+            //     for (int i = 0; i < board.Length; i++)
+            //     {
+            //         var id = Array.IndexOf(board[i], n);
+            //         if(id > -1) {
+            //             return (i, id);
+            //         }
+            //     }
+            //     return (-1, -1);
+            // }
+
+
+            int[][] parseBoards(IEnumerator<string> linesEnumerator)
+            {
+                int[][] board = new int[5][];
+                for (int i = 0; i < 5 && linesEnumerator.MoveNext(); i++)
+                {
+                    var strings = linesEnumerator.Current.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    board[i] = strings.Select(x => Convert.ToInt32(x)).ToArray();
+                }
+                return board;
+            }
+
+        }
     }
-}
 }
